14b3f1c79fb6bc907da355752edb27e745fb99ea
Chidi Eze <chidi.ezekwonna@gmail.com>
2022-10-10 11:52:29 +0100

    account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './my-first/my-first.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
